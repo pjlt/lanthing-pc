1662f32fcabaad6b20a724f36d9643efbcd46455
Zhennan Tu <zhennan.tu@gmail.com>
2023-10-10 06:31:05 +0800

    Update build.ps1 and win-build.yml

--- build.ps1
+++ build.ps1
@@ -7,219 +7,97 @@ function exit_if_fail {
     }
 }
 
-function build_protobuf() {
-    Write-Host -ForegroundColor Green building google-protobuf
-    Set-Location third_party/protobuf
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build2
-    Set-Location build2
-    Invoke-Expression "cmake ../ -DBUILD_SHARED_LIBS=ON -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_WITH_ZLIB=OFF"
-    Invoke-Expression "cmake --build . --config Debug"
-    Invoke-Expression "cmake --install . --config Debug --prefix install/Debug"
-    Invoke-Expression "cmake --build . --config Release"
-    Invoke-Expression "cmake --install . --config Release --prefix install/Release"
-    Set-Location $root_dir
-}
-
-function clear_protobuf {
-    Remove-Item -Force -Recurse third_party/protobuf/build2/*
-    exit_if_fail
-}
-
-function build_google_test() {
-    Write-Host -ForegroundColor Green building google-test
-    Set-Location third_party/googletest;
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake ../ -Dgtest_force_shared_crt=ON"
-    Invoke-Expression "cmake --build . --config Debug"
-    Invoke-Expression "cmake --install . --config Debug --prefix install/Debug"
-    
-    Invoke-Expression "cmake --build . --config Release"
-    Invoke-Expression "cmake --install . --config Release --prefix install/Release"
-    Set-Location $root_dir
-}
-
-function clear_google_test() {
-    Remove-Item -Force -Recurse third_party/googletest/build/*
-    exit_if_fail
-}
-
-function build_mbedtls() {
-    Write-Host -ForegroundColor Green building MbedTLS
-    Set-Location third_party/mbedtls
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DENABLE_TESTING=OFF -DENABLE_PROGRAMS=OFF -DDISABLE_PACKAGE_CONFIG_AND_INSTALL=OFF"
-    Invoke-Expression "cmake --build . --config Debug"
-    Invoke-Expression "cmake --install . --config Debug --prefix install/Debug"
-    Invoke-Expression "cmake --build . --config Release"
-    Invoke-Expression "cmake --install . --config Release --prefix install/Release"
-    Set-Location $root_dir
-}
-
-function clear_mbedtls() {
-    Remove-Item -Force -Recurse third_party/mbedtls/build/*
-    exit_if_fail
-}
-
-function build_libuv() {
-    Write-Host -ForegroundColor Green building libuv
-    Set-Location third_party/libuv
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DLIBUV_BUILD_TESTS=OFF -DLIBUV_BUILD_BENCH=OFF"
-    Invoke-Expression "cmake --build . --config Debug"
-    Invoke-Expression "cmake --install . --config Debug --prefix install/Debug"
-    Invoke-Expression "cmake --build . --config Release"
-    Invoke-Expression "cmake --install . --config Release --prefix install/Release"
-    Set-Location $root_dir
-}
-
-function clear_libuv() {
-    Remove-Item -Force -Recurse third_party/libuv/build/*
-    exit_if_fail
-}
-
-function build_sdl() {
-    Write-Host -ForegroundColor Green building SDL
-    Set-Location third_party/SDL
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .."
-    Invoke-Expression "cmake --build . --config Debug"
-    Invoke-Expression "cmake --install . --config Debug --prefix install/Debug"
-    Invoke-Expression "cmake --build . --config Release"
-    Invoke-Expression "cmake --install . --config Release --prefix install/Release"
-    Set-Location $root_dir
-}
-
-function clear_sdl() {
-    Remove-Item -Force -Recurse third_party/SDL/build/*
-    exit_if_fail
-}
-
-function build_onevpl() {
-    Write-Host -ForegroundColor Green building oneVPL
-    Set-Location third_party/oneVPL
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DBUILD_DEV=ON -DBUILD_DISPATCHER=ON -DBUILD_TOOLS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_PREVIEW=OFF -DINSTALL_EXAMPLE_CODE=OFF -DBUILD_DISPATCHER_ONEVPL_EXPERIMENTAL=ON -DBUILD_TOOLS_ONEVPL_EXPERIMENTAL=OFF -DCMAKE_INSTALL_PREFIX=install/Debug"
-    Invoke-Expression "cmake --build . --config Debug --target install"
-    Invoke-Expression "cmake .. -DBUILD_DEV=ON -DBUILD_DISPATCHER=ON -DBUILD_TOOLS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_PREVIEW=OFF -DINSTALL_EXAMPLE_CODE=OFF -DBUILD_DISPATCHER_ONEVPL_EXPERIMENTAL=ON -DBUILD_TOOLS_ONEVPL_EXPERIMENTAL=OFF -DCMAKE_INSTALL_PREFIX=install/Release"
-    Invoke-Expression "cmake --build . --config Release --target install"
-    Set-Location $root_dir
-}
-
-function clear_onevpl() {
-    Remove-Item -Force -Recurse third_party/oneVPL/build/*
-    exit_if_fail
-}
-
-function build_opus() {
-    Write-Host -ForegroundColor Green building opus
-    Set-Location third_party/opus
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DCMAKE_INSTALL_PREFIX=install/Debug"
-    Invoke-Expression "cmake --build . --config Debug --target install"
-    Invoke-Expression "cmake .. -DCMAKE_INSTALL_PREFIX=install/Release"
-    Invoke-Expression "cmake --build . --config Release --target install"
-    Set-Location $root_dir
-}
-
-function clear_opus() {
-    Remove-Item -Force -Recurse third_party/opus/build/*
-    exit_if_fail
-}
-
-function build_g3log() {
-    Write-Host -ForegroundColor Green building g3log
-    Set-Location third_party/g3log
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DUSE_DYNAMIC_LOGGING_LEVELS=ON -DENABLE_VECTORED_EXCEPTIONHANDLING=OFF -DENABLE_FATAL_SIGNALHANDLING=OFF -DCMAKE_INSTALL_PREFIX=install/Debug"
-    Invoke-Expression "cmake --build . --config Debug --target install"
-    Invoke-Expression "cmake .. -DUSE_DYNAMIC_LOGGING_LEVELS=ON -DENABLE_VECTORED_EXCEPTIONHANDLING=OFF -DENABLE_FATAL_SIGNALHANDLING=OFF -DCMAKE_INSTALL_PREFIX=install/Release"
-    Invoke-Expression "cmake --build . --config Release --target install"
-    Set-Location $root_dir
-}
-
-function build_vigemclient() {
-    Write-Host -ForegroundColor Green building ViGEmClient
-    Set-Location third_party/ViGEmClient
-    New-Item -ItemType Directory -ErrorAction SilentlyContinue build
-    Set-Location build
-    Invoke-Expression "cmake .. -DCMAKE_INSTALL_PREFIX=install/Debug -DViGEmClient_DLL=ON"
-    Invoke-Expression "cmake --build . --config Debug --target install"
-    Invoke-Expression "cmake .. -DCMAKE_INSTALL_PREFIX=install/Release -DViGEmClient_DLL=ON"
-    Invoke-Expression "cmake --build . --config Release --target install"
-    Set-Location $root_dir
-}
-
-function clear_vigemclient() {
-    Remove-Item -Force -Recurse third_party/ViGEmClient/build/*
-    exit_if_fail
-}
-
-function clear_g3log() {
-    Remove-Item -Force -Recurse third_party/g3log/build/*
-    exit_if_fail
-}
-
-function cmake_project() {
-    Invoke-Expression "cmake -B build/$script:build_type -DCMAKE_BUILD_TYPE=$script:build_type -DCMAKE_INSTALL_PREFIX=install/$script:build_type"
+function cmake_configure() {
+    Invoke-Expression "cmake -B build/$script:target_type -DCMAKE_BUILD_TYPE=$script:target_type -DCMAKE_INSTALL_PREFIX=install/$script:target_type"
     exit_if_fail
 }
 
 function cmake_build() {
-    Invoke-Expression "cmake --build build/$script:build_type --config $script:build_type --target install"
+    Invoke-Expression "cmake --build build/$script:target_type --config $script:target_type --target install"
     exit_if_fail
 }
 
-function check_build_type() {
-    if ($script:build_type -eq "debug") {
-        $script:build_type = "Debug"
+function cmake_clean() {
+    Invoke-Expression "cmake --build build/$script:target_type --target clean"
+}
+
+function check_target_type() {
+    if ($script:target_type -eq "debug") {
+        $script:target_type = "Debug"
         Write-Host -ForegroundColor Green Debug
-    } elseif ($script:build_type -eq "release") {
+    } elseif ($script:target_type -eq "release") {
         Write-Host -ForegroundColor Green Release
-        $script:build_type = "Release"
+        $script:target_type = "Release"
     } else {
-        Write-Host -ForegroundColor Red 'Please specify build type [Debug|Release]'
+        Write-Host -ForegroundColor Red 'Please specify target type [ Debug | Release ]'
         Exit -1
     }
 }
 
+class BuiltLib {
+    [string]$Name
+    [string]$Uri
+
+    BuiltLib([string]$n, [string]$u) {
+        $this.Uri = $u
+        $this.Name = $n
+    }
+}
+
+function prebuilt_fetch() {
+    $libs = @(
+        [BuiltLib]::new("mbedtls", "https://github.com/numbaa/mbedtls-build/releases/download/v3.2.1-1/mbedtls.zip"),
+        [BuiltLib]::new("sdl", "https://github.com/numbaa/sdl-build/releases/download/v2.0.20-1/sdl.zip"),
+        [BuiltLib]::new("vigemclient", "https://github.com/numbaa/vigemclient-build/releases/download/v1/vigemclient.zip"),
+        [BuiltLib]::new("libuv", "https://github.com/numbaa/libuv-build/releases/download/v1.44.1-1/libuv.zip"),
+        [BuiltLib]::new("onevpl", "https://github.com/numbaa/onevpl-build/releases/download/v2023.3.1-1/onevpl.zip"),
+        [BuiltLib]::new("opus", "https://github.com/numbaa/opus-build/releases/download/v1.4-1/opus.zip"),
+        [BuiltLib]::new("g3log", "https://github.com/numbaa/g3log-build/releases/download/v2.3-1/g3log.zip"),
+        [BuiltLib]::new("googletest", "https://github.com/numbaa/googletest-build/releases/download/v1.13.0-1/googletest.zip"),
+        [BuiltLib]::new("ffmpeg", "https://github.com/numbaa/ffmpeg-build/releases/download/v4.4.4-2/ffmpeg.zip"),
+        [BuiltLib]::new("protobuf", "https://github.com/numbaa/protobuf-build/releases/download/v3.24.3-1/protobuf.zip")
+    )
+
+    New-Item -ItemType Directory -ErrorAction SilentlyContinue third_party/prebuilt
+
+    foreach ($lib in $libs) {
+        $LibName = $lib.Name
+        Invoke-WebRequest -Uri $lib.Uri -OutFile ./third_party/prebuilt/$LibName.zip
+        Expand-Archive ./third_party/prebuilt/$LibName.zip -DestinationPath ./third_party/prebuilt/$LibName
+    }
+}
+
+function prebuilt_clean() {
+    Remove-Item -Force -Recurse third_party/prebuilt
+}
+
+function print_usage() {
+    Write-Host -ForegroundColor Green 'Usage: '
+    Write-Host -ForegroundColor Green '    build.ps1 prebuilt [ fetch | clean ]'
+    Write-Host -ForegroundColor Green '    build.ps1 build [ Debug | Release ]'
+    Write-Host -ForegroundColor Green '    build.ps1 clean [ Debug | Release ]'
+    Exit -1
+}
+
 $action = $args[0]
 
-if ($action -eq "prebuild") {
-    build_protobuf
-    build_google_test
-    build_mbedtls
-    build_libuv
-    build_sdl
-    build_onevpl
-    build_opus
-    build_g3log
-    build_vigemclient
+if ($action -eq "prebuilt") {
+    if ($args[1] -eq "fetch") {
+        prebuilt_fetch
+    } elseif ($args[1] -eq "clean") {
+        prebuilt_clean
+    } else {
+        Write-Host -ForegroundColor Red 'Please specify prebuilt type [ fetch | clean ]'
+        Exit -1
+    }
 } elseif ($action -eq "clean") {
-    clear_protobuf
-    clear_google_test
-    clear_mbedtls
-    clear_libuv
-    clear_sdl
-    clear_onevpl
-    clear_opus
-    clear_g3log
-    clear_vigemclient
+    check_target_type
+    cmake_clean
 } elseif ($action -eq "build") {
-    $script:build_type=$args[1]
-    check_build_type
+    $script:target_type=$args[1]
+    check_target_type
     cmake_project
     cmake_build
 } else {
-    Write-Host -ForegroundColor Green 'Usage: '
-    Write-Host -ForegroundColor Green '    build.ps1 [prebuild][build][clean]'
-    Write-Host -ForegroundColor Green '    build.ps1 prebuild'
-    Write-Host -ForegroundColor Green '    build.ps1 build [Debug|Release]'
-    Write-Host -ForegroundColor Green '    build.ps1 clean'
+    print_usage
 }
