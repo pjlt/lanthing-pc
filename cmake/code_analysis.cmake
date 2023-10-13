eb79e4ed52833d947173311fc3c3840bbdaeac11
Zhennan Tu <zhennan.tu@gmail.com>
2023-10-13 14:58:33 +0800

    Fix compile error

--- cmake/code_analysis.cmake
+++ cmake/code_analysis.cmake
@@ -2,7 +2,7 @@ function(set_code_analysis target enable)
 if (LT_WINDOWS)
     target_compile_options(${target} PRIVATE /W4 /WX /wd4819 /wd4251 /wd6326) #c4251这个警告跟dll导出有关，如果整个项目的编译平台、编译工具链、运行时都是一样的，理论上不会有问题
     if (${enable})
-        target_compile_options(${target} PRIVATE /external:W0 /external:I ${CMAKE_CURRENT_SOURCE_DIR}/third_party /analyze:rulesetdirectory ${LT_ROOT_DIR}/code_analysis/rulesets /analyze:external-)
+        target_compile_options(${target} PRIVATE /external:W0 /external:I ${CMAKE_CURRENT_SOURCE_DIR}/third_party /analyze:external-)
     endif()
 else()
     target_compile_options(${target} PRIVATE -Wall -Wextra -pedantic -Werror)
