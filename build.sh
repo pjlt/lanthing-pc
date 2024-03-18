d60f4bd46dc95908cebbe4d9d98fd1dd45d9e41e
Zhennan Tu <zhennan.tu@gmail.com>
2024-03-19 05:18:14 +0800

    Update rtc v0.7.3 => v0.7.4

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ check_build_type() {
 }
 
 rtc_fetch() {
-    rtc_url="https://github.com/numbaa/rtc-prebuilt/releases/download/v0.7.3/rtc.linux.zip"
+    rtc_url="https://github.com/numbaa/rtc-prebuilt/releases/download/v0.7.4/rtc.linux.zip"
     mkdir -p ./transport/rtc/linux
     echo "Fetch $rtc_url"
     wget $rtc_url -O ./third_party/prebuilt/rtc.linux.zip
